5bf630bc1c21da14843c5cad939e0eed4015e10c
rajendra chavan <rajendra.chavan@rksv.in>
2020-08-24 18:55:39 +0530

    Added boost

--- cmake/Versions.cmake
+++ cmake/Versions.cmake
@@ -1,3 +1,11 @@
+set(BOOST_VERSION "1.73.0")
+string(REPLACE "." "_" BOOST_VERSION_UNDERSCORE ${BOOST_VERSION})
+
+#set(BOOST_URL "https://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION_UNDERSCORE}.tar.bz2/download")
+#set(BOOST_HASH "SHA1=6d6ed02b29c860fd21b274fc4e1f820855e765e9")
+set(BOOST_URL "https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORE}.tar.bz2")
+set(BOOST_HASH "SHA256=4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402")
+
 set(UWEBSOCKETS_URL "https://github.com/uNetworking/uWebSockets.git")
 set(UWEBSOCKETS_TAG "v18.6.0")
 
